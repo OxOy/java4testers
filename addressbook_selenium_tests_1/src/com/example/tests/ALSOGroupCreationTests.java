14f3b4db41536381e7a063cb47d5c27e3c42aee1
OxOy <alex251089@yandex.ru>
2015-08-31 23:43:27 +0600

    addressbook_selenium_tests_1_CreateGroup_DZ1
    Первый проект.
    Тест по созданию группы для группировки контактов
    Домашнее задание 1.

---
+++ addressbook_selenium_tests_1/src/com/example/tests/ALSOGroupCreationTests.java
@@ -0,0 +1,31 @@
+package com.example.tests;
+
+//import org.junit.*;
+import org.testng.annotations.Test;
+
+public class ALSOGroupCreationTests extends TestBase{
+
+  @Test
+  public void testNonEmptyGroupCreation() throws Exception {
+	  openMainPage();
+	  gotoGroupsPage();
+	  initGroupCreation();
+	  GroupData group = new GroupData();
+	  group.group_name = "group_1";
+	  group.group_header = "header_1";
+	  group.group_footer = "footer_1";
+	  fillGroupForm(group);
+	  submitGroupCreation();
+	  returnToGroupsPage();
+  }
+  
+  @Test
+  public void testEmptyGroupCreation() throws Exception {
+	  openMainPage();
+	  gotoGroupsPage();
+	  initGroupCreation();
+	  fillGroupForm(new GroupData("", "", ""));
+	  submitGroupCreation();
+	  returnToGroupsPage();
+  }
+}
