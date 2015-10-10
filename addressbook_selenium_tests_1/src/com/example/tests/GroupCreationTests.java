82d58137155ffe40b400c83330e05bc786aa0746
OxOy <alex251089@yandex.ru>
2015-10-10 21:25:16 +0600

    урок 3 задание 1: ДЗ5
    результат выполнения задания ДЗ5
    Правда немного увлекся и частично уже сделал ДЗ7

--- addressbook_selenium_tests_1/src/com/example/tests/GroupCreationTests.java
+++ addressbook_selenium_tests_1/src/com/example/tests/GroupCreationTests.java
@@ -1,29 +1,35 @@
 package com.example.tests;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class GroupCreationTests extends TestBase {
-  @Test
-  public void testNonEmptyGroupCreation() throws Exception {
+  
+  @Test(dataProvider = "randomValidGroupGenerator")
+  public void testNonEmptyGroupCreation(GroupData group) throws Exception {
 	  app.getNavigationHelper().openMainPage();
 	  app.getNavigationHelper().gotoGroupsPage();
+	  
+	  //save old state
+	  List<GroupData> oldList = app.getGroupHelper().getGroups();
+	  
+	  //actions
 	  app.getGroupHelper().initGroupCreation();
-	  GroupData group = new GroupData();
-	  group.name = "group_1";
-	  group.header = "header_1";
-	  group.footer = "footer_1";
 	  app.getGroupHelper().fillGroupForm(group);
 	  app.getCommonHelper().submitData("submit");
 	  app.getGroupHelper().returnToGroupsPage();
+	  
+	  //save new state
+	  List<GroupData> newList = app.getGroupHelper().getGroups();
+
+	  // compare states
+	  //assertEquals(newList.size(), oldList.size() + 1);
+	  
+	  oldList.add(group);
+	  Collections.sort(oldList);
+	  assertEquals(newList, oldList);
   }
-  
-  /*@Test
-  public void testEmptyGroupCreation() throws Exception {
-	  openMainPage();
-	  gotoGroupsPage();
-	  initGroupCreation();
-	  fillGroupForm(new GroupData("", "", ""));
-	  submitGroupCreation();
-	  returnToGroupsPage();
-  }*/
 }
